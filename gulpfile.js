1dfe8cdef912785e526c8565971970bd465065b5
Pablo-Gites <pawel.zdybal.84@gmail.com>
2025-09-08 19:36:54 +0000

    Migrating to new gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,30 @@
-﻿/// <binding Clean='clean' />
-"use strict";
-
-const gulp = require("gulp"),
+﻿const gulp = require("gulp"),
       rimraf = require("rimraf"),
       concat = require("gulp-concat"),
       cleanCSS = require("gulp-clean-css"),
-      uglify = require("gulp-uglify");
+      uglify = require("gulp-uglify"),
+      sass = require("gulp-sass")(require("sass"));
 
 const paths = {
-  webroot: "./Tailspin.SpaceGame.Web/wwwroot/"
+  webroot: "./Tailspin.SpaceGame.Web/wwwroot/",
+  js: "./Tailspin.SpaceGame.Web/wwwroot/js/**/*.js",
+  minJs: "./Tailspin.SpaceGame.Web/wwwroot/js/**/*.min.js",
+  css: "./Tailspin.SpaceGame.Web/wwwroot/css/**/*.css",
+  minCss: "./Tailspin.SpaceGame.Web/wwwroot/css/**/*.min.css",
+  scss: "./Tailspin.SpaceGame.Web/wwwroot/scss/**/*.scss",
+  concatJsDest: "./Tailspin.SpaceGame.Web/wwwroot/js/site.min.js",
+  concatCssDest: "./Tailspin.SpaceGame.Web/wwwroot/css/site.min.css"
 };
-
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
 
 gulp.task("clean:js", done => rimraf(paths.concatJsDest, done));
 gulp.task("clean:css", done => rimraf(paths.concatCssDest, done));
-gulp.task("clean", gulp.series(["clean:js", "clean:css"]));
+gulp.task("clean", gulp.series("clean:js", "clean:css"));
+
+gulp.task("compile:sass", () => {
+  return gulp.src(paths.scss)
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(gulp.dest(paths.webroot + "css"));
+});
 
 gulp.task("min:js", () => {
   return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
@@ -36,9 +40,6 @@ gulp.task("min:css", () => {
     .pipe(gulp.dest("."));
 });
 
-gulp.task("min", gulp.series(["min:js", "min:css"]));
-
-gulp.task('proj:publish', gulp.series('min'))
-
-// A 'default' task is required by Gulp v4
-gulp.task("default", gulp.series(["min"]));
+gulp.task("min", gulp.series("compile:sass", "min:js", "min:css"));
+gulp.task("proj:publish", gulp.series("clean", "min"));
+gulp.task("default", gulp.series("min"));
